e5045813bb43621449ed589c1629c4612788b3a2
jiayuancs <jiayuancs@yeah.net>
2026-02-09 22:45:41 +0800

    support OOD output

--- hola_model.py
+++ hola_model.py
@@ -1324,7 +1324,13 @@ class UPT(nn.Module):
             detections.append(dict(
                 boxes=bx, pairing=torch.stack([h[x], o[x]]),
                 scores=scores * pr[x, y], labels=y,
-                objects=obj[x], size=size
+                objects=obj[x], size=size,
+                # 用于OOD评测：
+                # all_scores[i] 表示第 i 个人物对(非重复人物对)的动作类别概率分布
+                all_scores = torch.sigmoid(lg) * pr,
+                # all_pairings[i] 表示第 i 个人物对(非重复人物对)的边界框索引
+                all_pairings = torch.stack([h, o]),
+                all_objects = obj
             ))
 
         return detections
